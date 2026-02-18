3072eb3309feb8ad40afe69ab943c7d5d85321bf
sibir <sibir@SIBI>
2026-02-18 15:08:11 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    //public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    //{
+       public DbSet<AppUser> Users { get; set; }
+    // }
+
+
+
+}
+
